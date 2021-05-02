8efd18224eecff48993a0d99f7b233791676927f
marouenes <marouane.skandaji@gmail.com>
2021-05-03 01:07:09 +0200

    Test out SES Messages

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const orderManager = require('./orderManager');
+const kinesisHelper = require('./kinesisHelper'); 
+const producerManager = require('./producerManager');
 
 function createResponse(StatusCode, message) {
   const response = {
@@ -32,5 +34,21 @@ module.exports.orderFulfillement = async (event) => {
     return createResponse(200, `Order with orderId:${orderId} was sent to delivery`);
   }).catch(error => {
     return createResponse(400, error);
+  })
+}
+
+module.exports.notfiyProducer = async (event) => {
+  const record = kinesisHelper.getRecords(event);
+
+  const ordersPlaced = records.filter(r => r.eventType == 'order_placed');
+
+  if (ordersPlaced <= 0) {
+   return 'There is nothing';
+  }
+
+  producerManager.handlePlacedOrders(ordersPlaced).then(() => {
+    return 'Everything went well!'
+  }).catch(error => {
+    return errror;
   })
 }
---
+++ producerManager.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const AWS = require('aws-sdk');
+const ses = new AWS.SES({
+    region: process.env.region
+});
+
+const PRODUCER_EMAIL = process.env.producerEmail;
+const ORDERING_SYSTEM_EMAIL = process.env.orderingSystemEmail;
+
+module.exports.handlePlacedOrders = ordersPlaced => {
+    var ordersPlacedPromises = [];
+
+    for (let order of ordersPlaced) {
+        const temp = notifyProducerByEmail(order);
+        ordersPlacedPromises.push(temp);
+    }
+
+    return Promise.all(ordersPlacedPromises);
+}
+
+function notifyProducerByEmail(order) {
+    const params = {
+        Destination: {
+            ToAddresses: [PRODUCER_EMAIL]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: JSON.stringify(order)
+                }
+            },
+            Subject: {
+                Data: 'New order'
+            }   
+        },
+        Source: ORDERING_SYSTEM_EMAIL
+    };
+
+    return ses.sendEmail(params).promise().then((data) => {
+        return data;
+    });
+} 
